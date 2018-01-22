281750b0e429251e9c820d1758c2eaf2b477b09e
caozhihui <caozhihui@4399inc.com>
2018-01-22 14:21:41 +0800

    活动

--- routers/scheduling.js
+++ routers/scheduling.js
@@ -81,11 +81,11 @@ router.post('/member/edit',async (req,res,next)=>{
 
     let updateData = JSON.parse(JSON.stringify(req.body));
     delete updateData.mid;
-    SchedulingUser.update(updateData,{ //更新userInfo
+    SchedulingUser.update({ //更新userInfo
         where : {
             mid : mid
         }
-    });
+    },updateData);
     return res.send(res.stackResponse(100, 'success', {}));
 });
 
